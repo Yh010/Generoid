e4c4a25c2decfcc89bdd5e15ba0675478610b073
Yash Hegde <yash4success@gmail.com>
2024-11-21 17:08:43 +0530

    prisma schema ini

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,9 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google";
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
+
 //TODO: when deploying, change the call back urls and authorized urls to the deployed domain
 export const handler = NextAuth({
     providers: [
@@ -7,7 +11,23 @@ export const handler = NextAuth({
         clientId: process.env.GOOGLE_CLIENT_ID || "",
         clientSecret: process.env.GOOGLE_CLIENT_SECRET || ""
       })
-    ],
+  ],
+  callbacks: {
+    async signIn({ profile }) {
+      const existingUser = await prisma.generoidUsers.findUnique({
+        where: { email: profile?.email }
+      })
+      if (!existingUser) {
+        await prisma.generoidUsers.create({
+          data: {
+            email: profile?.email,
+            name: profile?.name,
+          }
+        })
+      }
+      return true
+    }
+  },
   secret: process.env.NEXTAUTH_SECRET
 })
 
